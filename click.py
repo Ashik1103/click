5350da712ecbb6146b6c4d2c2c8d2d315d0dafd3
Ashik <haja.ashik.sg@gretail.com>
2024-07-23 18:04:51 +0530

    test

---
+++ click.py
@@ -0,0 +1,21 @@
+import pyautogui as pg
+import keyboard
+import time
+
+print("move the mouse pointer to the circle and click enter")
+
+while True:
+    if keyboard.read_key() == "enter":
+        mousePos = pg.position()
+        break
+print("starting the clicker")
+time.sleep(5)
+print("started the clicker")
+
+while True:
+    pg.click(mousePos.x, mousePos.y)
+    print("clicked")
+    time.sleep(5)
+
+    if keyboard.is_pressed("q"):
+        break
