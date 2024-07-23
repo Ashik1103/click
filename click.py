453198b4ae001a3a52245fb605451a56fc01cb4b
Ashik <haja.ashik.sg@gretail.com>
2024-07-24 00:07:56 +0530

    feat: test

--- click.py
+++ click.py
@@ -2,21 +2,24 @@ import pyautogui as pg
 import keyboard
 import time
 
-print("move the mouse pointer to the circle and click enter")
+# print("move the mouse pointer to the circle and click enter")
 
-while True:
-    if keyboard.read_key() == "enter":
-        mousePos = pg.position()
-        break
-print("starting the clicker")
-time.sleep(5)
-print("started the clicker")
+# while True:
+#     if keyboard.read_key() == "enter":
+#         mousePos = pg.position()
+#         break
+# print("starting the clicker")
+# time.sleep(5)
+# print("started the clicker")
 
-while True:
-    print(mousePos.x, mousePos.y)
-    pg.click(mousePos.x, mousePos.y)
-    print("clicked")
-    # time.sleep(5)
+# while True:
+#     print(mousePos.x, mousePos.y)
+#     pg.click(mousePos.x, mousePos.y)
+#     print("clicked")
+#     # time.sleep(5)
 
-    if keyboard.is_pressed("q"):
-        break
+#     if keyboard.is_pressed("q"):
+#         break
+print(pg.position())  # Prints current mouse position
+pg.moveTo(100, 100)
+pg.click()
